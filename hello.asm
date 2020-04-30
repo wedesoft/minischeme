0b26017f0d21f39e14459460f89e74ba3bb29e71
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-04-30 23:28:46 +0100

    Removed some comments

--- hello.asm
+++ hello.asm
@@ -1,9 +1,4 @@
-; ----------------------------------------------------------------------------------------
 ; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
-; To assemble and run:
-;
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
-; ----------------------------------------------------------------------------------------
 
           global    _start
 
