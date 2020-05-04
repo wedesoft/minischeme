c7947fcc74710e0388f6df6ceeae4928980ad61f
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-05-04 22:47:52 +0100

    Read number from stream

--- test_tokenizer.c
+++ test_tokenizer.c
@@ -10,7 +10,17 @@ static MunitResult test_empty_stream(const MunitParameter params[], void *data) 
   return MUNIT_OK;
 }
 
+static MunitResult test_number(const MunitParameter params[], void *data) {
+  FILE *f = fmemopen(" 123", 4, "r");
+  FT t = token(f);
+  munit_assert_true(is_number(t));
+  munit_assert_int(ft_to_int(t), ==, 123);
+  fclose(f);
+  return MUNIT_OK;
+}
+
 MunitTest test_tokenizer[] = {
   {"/empty_stream", test_empty_stream, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/number"      , test_number      , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
   {NULL           , NULL             , NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 };
--- tokenizer.c
+++ tokenizer.c
@@ -1,7 +1,29 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include "tokenizer.h"
 
 
 FT token(FILE *stream) {
   FT result = UNDEFINED;
+  while (true) {
+    int c = getc(stream);
+    if (c == 0 || c == EOF)
+      return result;
+    if (!isspace(c)) {
+      ungetc(c, stream);
+      break;
+    };
+  };
+  while (true) {
+    int c = getc(stream);
+    if ((result == UNDEFINED || is_number(result) && isdigit(c))) {
+      if (result == UNDEFINED)
+        result = ft_from_int(0);
+      result = ft_from_int(ft_to_int(result) * 10 + c - '0');
+    } else if (isspace(c) || c == 0 || c == EOF) {
+      ungetc(c, stream);
+      return result;
+    };
+  };
   return result;
 }
